84278c99011d544b1b2149fd27727d51600df20d
eric.zhai <896895240@qq.com>
2019-11-26 17:27:52 +0800

    根据组名，从zabbix中拉取主机的监控项及触发器

--- Practice_Python/zabbix_helper.py
+++ Practice_Python/zabbix_helper.py
@@ -1,5 +1,29 @@
 from pyzabbix import ZabbixAPI
 import xlwt
+import pandas as pd
+import os
+import platform
+
+projectName= 'qqq'
+if platform.system() == 'Windows':
+    SRC = "C:\\Users\\eric.zhai\\Desktop\\{}".format(projectName)
+    DEST = "C:\\Users\\eric.zhai\\Desktop\\{}-new".format(projectName)
+    srcExists = os.path.exists(SRC)
+    destExists = os.path.exists(DEST)
+    if not srcExists:
+        os.makedirs(SRC)
+    if not destExists:
+        os.makedirs(DEST)
+else:
+    SRC='/tmp/{}'.format(projectName)
+    DEST='/tmp/{}-new'.format(projectName)
+    srcExists = os.path.exists(SRC)
+    destExists = os.path.exists(DEST)
+    if not srcExists:
+        os.makedirs(SRC)
+    if not destExists:
+        os.makedirs(DEST)
+
 
 class pyZabbixApi(object):
     def __init__(self):
@@ -22,65 +46,109 @@ class pyZabbixApi(object):
         return hosts
 
     def getItemAndTrigerFromHost(self, zapi, hosts):
+        '''
+        oneHostData:存储一台主机的信息
+        oneItemTriData：存储一条含有触发器的监控项信息
+        oneItemData：存储一条监控项信息
+        oneTriData:一条触发器信息
+        '''
         for host in hosts:
             items = zapi.item.get(hostids=host['hostid'],
                                   output=['name', 'key_'])
-            onedata = list()
-            onedata.append(['主机IP', '监控项ID', '监控项描述', '监控项键值', '触发器ID', '触发器表达式',
+            #存储主机List
+            oneHostData = list()
+            oneHostData.append(['主机IP', '监控项ID', '监控项描述', '监控项键值', '触发器ID', '触发器表达式',
                            '触发器描述', '告警等级','触发器停用与否'])
             for item in items:
                 triggers = zapi.trigger.get(itemids=item['itemid'], output=['expression',
                                                                                'description',
                                                                                'priority','status'],expandExpression='True')
-
-                data = list()
-                data.append(host['host'])
-                dataTemp = list()
-                dataTemp.extend(list(item.values()))
+                #存储一条含有触发器的监控项
+                oneItemTriData = list()
+                oneItemTriData.append(host['host'])
+                #存储一条监控项
+                oneItemData = list()
+                oneItemData.extend(list(item.values()))
 
                 for trigger in triggers:
-                    trigger_data = list(trigger.values())
-                    dataTemp.extend(trigger_data)
-                    data.extend(dataTemp)
-                    onedata.append(data)
-            print(onedata)
-            write_in_excel(onedata)
+                    oneTriData = list(trigger.values())
+                    oneItemTriDataTemp = oneItemTriData.copy()
+                    oneItemDataTemp = oneItemData.copy()
+                    oneItemDataTemp.extend(oneTriData)
+                    oneItemTriDataTemp.extend(oneItemDataTemp)
+                    oneHostData.append(oneItemTriDataTemp)
+            write_in_excel(oneHostData)
 
 def write_in_excel(result):
+    global SRC
     try:
         book = xlwt.Workbook()
         sheet = book.add_sheet(result[1][0])
         for row in range(len(result)):
             for col in range(len(result[0])):
                 sheet.write(row, col, result[row][col])
-        book.save('{}.xls'.format(result[1][0]))
+        if platform.system() == 'Windows':
+            book.save(SRC + '\\' + '{}.xls'.format(result[1][0]))
+        else:
+            book.save(SRC + '/' + '{}.xls'.format(result[1][0]))
     except  IndexError:
-        print('这个机器没有触发器，请确认')
+        print('no trigger')
+
+def formatExcel(filepath):
+    global DEST
+    df=pd.read_excel(filepath)
+
+    # 去除监控项键值列中的基础监控项
+    df = df[~ df['监控项键值'].str.contains('agent.hostname', case=False)]
+    df = df[~ df['监控项键值'].str.contains('agent.ping', case=False)]
+    df = df[~ df['监控项键值'].str.contains('agent.version', case=False)]
+    df = df[~ df['监控项键值'].str.contains('proc.num\[,,run\]', case=False)]
+    df = df[~ df['监控项键值'].str.contains('proc.num\[\]', case=False)]
+    df = df[~ df['监控项键值'].str.contains('system.cpu.load\[all,avg1\]', case=False)]
+    df = df[~ df['监控项键值'].str.contains('system.cpu.num', case=False)]
+    df = df[~ df['监控项键值'].str.contains('system.cpu.load\[all,avg5\]', case=False)]
+    df = df[~ df['监控项键值'].str.contains('system.cpu.util\[,idle\]', case=False)]
+    df = df[~ df['监控项键值'].str.contains('system.cpu.util\[,iowait\]', case=False)]
+    df = df[~ df['监控项键值'].str.contains('system.hostname', case=False)]
+    df = df[~ df['监控项键值'].str.contains('system.uname', case=False)]
+    df = df[~ df['监控项键值'].str.contains('system.uptime', case=False)]
+    df = df[~ df['监控项键值'].str.contains('vfs', case=False)]
+    df = df[~ df['监控项键值'].str.contains('vm.memory.size\[available\]', case=False)]
+    df = df[~ df['监控项键值'].str.contains('kernel.maxfiles', case=False)]
+    df = df[~ df['监控项键值'].str.contains('kernel.maxproc', case=False)]
+
+    #去除已停用的触发器
+    df=df[~df['触发器停用与否'].isin([1])]
+    #去除无用列
+    df = df.drop(['监控项ID'],axis=1)
+    df = df.drop(['触发器ID'],axis=1)
+    df = df.drop(['触发器停用与否'],axis=1)
+    # print(df)
+    if platform.system() == 'Windows':
+        fileName = filepath.split("\\")
+        df.to_excel(DEST + '\\' + 'new-' + fileName[-1])
+    else:
+        fileName = filepath.split("/")
+        df.to_excel(DEST + '/' + 'new-' + fileName[-1])
 
 
 if __name__=='__main__':
+    #zabbix组名
+    zabbixGroupName = 'test'
+    projectName = 'qqq'
     r = pyZabbixApi()
     zapi = r.login()
-    group_id = r.getGroupIdByName(zapi, 'Onebank 性能测试 ZP 卡前置')
+    group_id = r.getGroupIdByName(zapi, '{}'.format(zabbixGroupName))
     hosts = r.getHostsFromGroupId(zapi,group_id)
     r.getItemAndTrigerFromHost(zapi,hosts)
 
+    src = "C:\\Users\\eric.zhai\\Desktop\\{}".format(projectName)
+    if platform.system() == 'Windows':
+        src = "C:\\Users\\eric.zhai\\Desktop\\{}".format(projectName)
+    else:
+        src = '/tmp/{}'.format(projectName)
 
-
-
-
-
-# a = [1,2,3]
-# b = [[4,4,4],[2,2,2]]
-# data = list()
-# d= list()
-# d.extend(a)
-#
-# for i in b:
-#     d1 = d.copy()
-#     f= list(i)
-#     d1.extend(f)
-#     data.append(d1)
-#     continue
-#
-# print(data)
+    files = os.listdir(src)
+    for file in files:
+        filepath = os.path.join(src,file)
+        formatExcel(filepath)
