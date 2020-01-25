7a267473be27cbb21d597b036c4153fd888d4b20
zll <896895240@qq.com>
2020-01-25 21:36:54 +0800

    ..

--- Practice_Python/bilibili_spider.py
+++ Practice_Python/bilibili_spider.py
@@ -3,40 +3,61 @@ import re
 
 import requests
 from bs4 import BeautifulSoup
+import os
 
-allUrl = []
+allVideoUrl = []
+allAudioUrl = []
+dir = './video/'
 
-for i in range(1,28):
+for i in range(1, 28):
     headers = {'Host': 'www.bilibili.com',
                'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36',
                'Connection': 'keep-alive',
                }
 
-    url = "http://www.bilibili.com/video/av79827258/?p={}".format(i)
+    url = "https://www.bilibili.com/video/av79827258/?p={}".format(i)
     html = requests.get(url=url, headers=headers).text
     soup = BeautifulSoup(html, "html.parser")
     pattern = re.compile(r'"backup_url"')
     script = soup.find("script", text=pattern).text.split('window.__playinfo__=')[1]
     jsonData = json.loads(script)
-    # print(jsonData['data']['dash']['video'][0])
-    baseUrl = jsonData['data']['dash']['video'][0]['baseUrl']
-    # backupUrlA = jsonData['data']['dash']['video'][0]['backupUrl'][0]
-    # backupUrlB = jsonData['data']['dash']['video'][0]['backupUrl'][1]
-    httpsUrl = 'https:' + baseUrl.split('http:')[1]
-    allUrl.append(httpsUrl)
+    videoBaseUrl = jsonData['data']['dash']['video'][0]['baseUrl']
+    audioBaseUrl = jsonData['data']['dash']['audio'][0]['baseUrl']
 
-print(allUrl)
-print(len(allUrl))
+    allVideoUrl.append(videoBaseUrl)
+    allAudioUrl.append(audioBaseUrl)
 
-for i in range(1,len(allUrl)+1):
-    print('第{}集视频开始下载'.format(i))
-    https_headers = {
-        'Origin': 'https://www.bilibili.com',
-        'Referer': 'https://www.bilibili.com/video/av79827258',
-        'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36',
-    }
-    video = requests.get(url=allUrl[i-1], headers=https_headers).content
-    # 视频保存在与py文件同级的video文件夹下
+print(allVideoUrl)
+print(len(allVideoUrl))
+print(allAudioUrl)
+print(len(allAudioUrl))
 
-    with open('./video/' + '{}' + '.mp4', 'wb+').format(i) as f:
+https_headers = {
+    'Origin': 'https://www.bilibili.com',
+    'Referer': 'https://www.bilibili.com/video/av79827258/',
+    'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36',
+}
+
+for i in range(20, 28):
+    # 下载视频
+    videoName = 'temp-video-{}'.format(i)
+    print("视频{}下载开始".format(videoName))
+    video = requests.get(url=allVideoUrl[i - 1], headers=https_headers).content
+    with open('./video/' + videoName + '.mp4', 'wb+') as f:
         f.write(video)
+    print("视频{}下载结束".format(videoName))
+
+    # 下载音频
+    audioName = 'temp-audio-{}'.format(i)
+    print("音频{}下载开始".format(audioName))
+    audio = requests.get(url=allAudioUrl[i - 1], headers=https_headers).content
+    with open('./video/' + audioName + '.mp4', 'wb+') as f:
+        f.write(audio)
+    print("音频{}下载结束".format(audioName))
+
+    print("合并开始:{}和{}".format(videoName, audioName))
+    finalName = 'CS229-{}'.format(i)
+    ss = 'cd %s && ffmpeg -i %s.mp4 -i %s.mp4 -c copy %s.mp4' % (dir, videoName, audioName, finalName)
+    os.system(ss)
+    print("合并结束：%s" % finalName)
+# https://zhuanlan.zhihu.com/p/96538085
